0488d2c66730895c72210b2073ddf4f828498380
Scott Kirkland <srkirkland@gmail.com>
2010-02-24 16:12:22 -0800

    added customer class

---
+++ ValidationResourceMessages/Models/Customer.cs
@@ -0,0 +1,11 @@
+namespace ValidationResourceMessages.Models
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string Title { get; set; }
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
